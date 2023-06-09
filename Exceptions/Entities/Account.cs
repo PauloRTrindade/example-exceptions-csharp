357b81d081c80a1df94ff108dd9ad0461a5678ad
PauloRTrindade <prtrindadesf@gmail.com>
2023-06-09 10:11:00 -0300

    Created class 'Account'

---
+++ Exceptions/Entities/Account.cs
@@ -0,0 +1,33 @@
+﻿namespace Exceptions.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"{Number}, {Holder}, {Balance}, {WithdrawLimit}";
+        }
+    }
+}
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Exceptions.Entities;
+using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter account data:");
+        Console.Write("Number: ");
+        int Number = int.Parse(Console.ReadLine());
+        Console.Write("Holder: ");
+        string Holder = Console.ReadLine();
+        Console.Write("Initial balance: ");
+        double Balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Withdraw limit: ");
+        double WithdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Account account = new Account(Number, Holder, Balance, WithdrawLimit);
+
+        Console.WriteLine();
+        Console.WriteLine(account);
+    }
+}
